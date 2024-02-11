b0498c596807e890907d817f16131e1f093bab11
sohel <sohelshaikh01231@gmail.com>
2024-02-11 14:24:14 +0530

    Updated Mobile Nav..

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import "./Header.css";
 
 import { NavLink } from "react-router-dom";
 
 function Header() {
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
+  const mobileNavStyle = {
+    opacity: mobileNavOpen ? 1 : 0,
+    transform: mobileNavOpen ? 'translateY(100%)' : 'translateY(0%)'
+  };
+
   return (
     <header className="header">
       <h2 className="logo">
         <NavLink to="/">Mo.Sohel</NavLink>
       </h2>
-      <span className="mobile-hamb-icon">
+      <span
+        className="mobile-hamb-icon"
+        onClick={() => setMobileNavOpen(!mobileNavOpen)}
+      >
         <i class="fa-solid fa-bars"></i>
       </span>
       <ul className="header-navigation">
@@ -19,6 +30,18 @@ function Header() {
           <NavLink to="/projects">Projects</NavLink>
         </li>
       </ul>
+      {mobileNavOpen && (
+        <div className="mobile-nav" style={mobileNavStyle}>
+          <ul className="mob-header-navigation">
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">Projects</NavLink>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
--- src/components/Homemain/Homemain.js
+++ src/components/Homemain/Homemain.js
@@ -1,31 +1,37 @@
-import './Homemain.css';
+import "./Homemain.css";
 
-import githubLogo from '../../Assets/logos/GitHub logo.png';
-import instagram from '../../Assets/logos/Instagram.png';
-import Lovefooter from '../Lovefooter/Lovefooter';
+import githubLogo from "../../Assets/logos/GitHub logo.png";
+import instagram from "../../Assets/logos/Instagram.png";
+import Lovefooter from "../Lovefooter/Lovefooter";
 
 function Homemain() {
   return (
     <div className="main-content">
       <p className="greeting-header">Hey There 👋🏻</p>
-      <div className='content-container'>
-        <h1 className='main-header'>My name is<br />Shaikh Mohammed Sohel </h1>
-        <p className='main-sub-header'>A Frontend Developer & a Keen Lerner</p>
-        <div className='main-button-container'>
-            <button className='readmore-btn'>Read more</button>
-            <button className='contct-btn'>Contact</button>
+      <div className="content-container">
+        <h1 className="main-header">
+          My name is
+          <br />
+          Shaikh Mohammed Sohel{" "}
+        </h1>
+        <p className="main-sub-header">A Frontend Developer & a Keen Lerner</p>
+        <div className="main-button-container">
+          <a href="/about">
+            <button className="readmore-btn">Read more</button>
+          </a>
+          <button className="contct-btn">Contact</button>
         </div>
       </div>
-      <div className='main-footer'>
-        <div className='footer-social-links'>
-            <a className='link' href='/'>
-                <img src={githubLogo} alt='github'/>
-                <span>sohel0602</span>
-            </a>
-            <a className='link' href='/'>
-                <img src={instagram} alt='instagram'/>
-                <span>sohelshaikh</span>
-            </a>
+      <div className="main-footer">
+        <div className="footer-social-links">
+          <a className="link" href="/">
+            <img src={githubLogo} alt="github" />
+            <span>sohel0602</span>
+          </a>
+          <a className="link" href="/">
+            <img src={instagram} alt="instagram" />
+            <span>sohelshaikh</span>
+          </a>
         </div>
         <Lovefooter />
       </div>
